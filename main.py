d7eec9309ca3cc26c23360fda9ad233a29fb412b
Utkarsh Kharayat <utkarshkhrt69@gmail.com>
2021-10-21 17:38:46 +0530

    v3.0.2 Added Spacy to trainer.py for NER Added displaCY to visualise NER

--- main.py
+++ main.py
@@ -4,60 +4,76 @@ from pdf2image import convert_from_path  # For scanned PDFs
 from PIL import Image
 import PyPDF2  # Convert text PDF to text
 import cv2
+import pytesseract
 
-r'''
-PDF_file = r'C:\Users\33669\PycharmProjects\OCR\pdf2img\test.pdf'
-#print(len(PDF_file))
-pages = convert_from_path(PDF_file, 500, poppler_path=r"C:\poppler-21.09.0\Library\bin", first_page=150, last_page=160, timeout=10000)
-#
-# # Counter to store images of each page of PDF to image
-image_counter = 1
-#
-# Iterate through all the pages stored above
-for page in pages:
-    # Declaring filename for each page of PDF as JPG
-    # For each page, filename will be:
-    # PDF page 1 -> page_1.jpg
-    # PDF page 2 -> page_2.jpg
-    # PDF page 3 -> page_3.jpg
-    # ....
-    # PDF page n -> page_n.jpg
-    filename = "page" + str(image_counter) + ".png"
+def pdf2img(PDF):
+    # print(len(PDF))
+    pages = convert_from_path(PDF, 500, poppler_path=r"C:\poppler-21.09.0\Library\bin", first_page=150,
+                              last_page=160, timeout=10000)
+    #
+    # # Counter to store images of each page of PDF to image
+    image_counter = 1
+    #
+    # Iterate through all the pages stored above
+    for page in pages:
+        # Declaring filename for each page of PDF as JPG
+        # For each page, filename will be:
+        # PDF page 1 -> page_1.jpg
+        # PDF page 2 -> page_2.jpg
+        # PDF page 3 -> page_3.jpg
+        # ....
+        # PDF page n -> page_n.jpg
+        filename = "page" + str(image_counter) + ".png"
 
-    # Save the image of the page in system
-    page.save(r'C:\Users\33669\PycharmProjects\OCR\pdf2img\ ' + filename)
-    print('Saved page number ' + str(image_counter))
-    # Increment the counter to update filename
-    image_counter = image_counter + 1
+        # Save the image of the page in system
+        page.save(r'C:\Users\33669\PycharmProjects\OCR\pdf2img\ ' + filename)
+        print('Saved page number ' + str(image_counter))
+        # Increment the counter to update filename
+        image_counter = image_counter + 1
 
-r'''
-img_loc = r'C:\Users\33669\PycharmProjects\OCR\page6.png'
-image = cv2.imread(img_loc)
-reader = easyocr.Reader(['en'],  recog_network='custom_example')  # , recog_network='custom_example' this needs to run only once to load the model into memory
-result = reader.readtext(img_loc, paragraph=True)#, rotation_info=[90, 180, 270] , mag_ratio=1.0, y_ths=0.05, x_ths=1.5, width_ths=1.5
-cv2.startWindowThread()
-for (bbox, text) in result:#, prob
-    # display the OCR'd text and associated probability
-    # print("[INFO] {:.4f}: {}".format(prob, text))
-    print(text)
-    # unpack the bounding box
-    (tl, tr, br, bl) = bbox
-    tl = (int(tl[0]), int(tl[1]))
-    tr = (int(tr[0]), int(tr[1]))
-    br = (int(br[0]), int(br[1]))
-    bl = (int(bl[0]), int(bl[1]))
-    # cleanup the text and draw the box surrounding the text along
-    # with the OCR'd text itself
-    cv2.rectangle(image, tl, br, (0, 0, 255), 4)
-    cv2.putText(image, text, (tl[0], tl[1] - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0, 90, 200), 8)
+def searchable_ocr(img):
+    pdf = pytesseract.image_to_pdf_or_hocr(img, config='--oem 1', extension='pdf')
+    print(pytesseract.image_to_string(img))
+    with open(r'C:\Users\33669\PycharmProjects\OCR\outputs\out_ocr1.pdf', 'w+b') as f:
+        f.write(pdf)
 
-file = open("OCR_OUT2.txt", 'w')
-for (bbox, text) in result:#, prob
-    file.write(str(text))
-    file.write('\n')
-file.close()
-# show the output image
-cv2.namedWindow('PDF Output', cv2.WINDOW_NORMAL)
-cv2.imshow("PDF Output", image)
-cv2.waitKey(0)
+def img_ocr(loc):
+    image = cv2.imread(loc)
+    reader = easyocr.Reader(['en'],
+                            recog_network='custom_example')  # , recog_network='custom_example' this needs to run only once to load the model into memory
+    result = reader.readtext(img_loc,
+                             paragraph=True)  # , rotation_info=[90, 180, 270], y_ths=1, x_ths=0.09, height_ths=0.5, ycenter_ths=0.5, width_ths=0.5
+    cv2.startWindowThread()
+    for (bbox, text) in result:  # , prob
+        # display the OCR'd text and associated probability
+        # print("[INFO] {:.4f}: {}".format(prob, text))
+        print(text)
+        # unpack the bounding box
+        (tl, tr, br, bl) = bbox
+        tl = (int(tl[0]), int(tl[1]))
+        tr = (int(tr[0]), int(tr[1]))
+        br = (int(br[0]), int(br[1]))
+        bl = (int(bl[0]), int(bl[1]))
+        # cleanup the text and draw the box surrounding the text along
+        # with the OCR'd text itself
+        cv2.rectangle(image, tl, br, (0, 0, 255), 4)
+        cv2.putText(image, text, (tl[0], tl[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0, 90, 200), 8)
+
+    file = open(r"C:\Users\33669\PycharmProjects\OCR\outputs\OCR_OUT2.txt", 'w')
+    for (bbox, text) in result:  # , prob
+        file.write(str(text))
+        file.write('\n')
+    file.close()
+    # show the output image
+    cv2.namedWindow('PDF Output', cv2.WINDOW_NORMAL)
+    cv2.imshow("PDF Output", image)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    PDF_file = r'C:\Users\33669\PycharmProjects\OCR\pdf2img\test.pdf'
+    img_loc = r'C:\Users\33669\PycharmProjects\OCR\K2.jpg'
+    img_ocr(img_loc)
+    # pdf2img(PDF_file)
+    # searchable_ocr(img_loc) # For converting image to text embedded PDF
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -6,8 +6,13 @@ from utils import AttrDict
 import pandas as pd
 from PIL import Image
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\33669\tesseract\tesseract.exe'
 
+import spacy
+from spacy import displacy
+import webbrowser
+import tempfile
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\33669\tesseract\tesseract.exe'
 cudnn.benchmark = True
 cudnn.deterministic = False
 
@@ -28,15 +33,21 @@ def get_config(file_path):
         opt.character = opt.number + opt.symbol + opt.lang_char
     os.makedirs(f'./saved_models/{opt.experiment_name}', exist_ok=True)
     return opt
+
+def train_ner():
+
+    nlp = spacy.load("en_core_web_trf", disable=["tagger", "parser", "attribute_ruler", "lemmatizer"])
+    doc = nlp(r"12.0 Cr was given to the company in compensation. In the period April 1 to September 20, 12.47 lakh cases of ticketless/irregular travellers were detected in suburban and non-suburban trains (long-distance trains) and an amount of `71.25 crore was realised as penalty. This is highest in terms of revenue among all zonal railways,” CR’s Chief Public Relations Officer Shivaji Sutar said.")
+    print([(ent.text, ent.label_) for ent in doc.ents])
+    visual = displacy.render(doc, style="ent")
+
+    with tempfile.NamedTemporaryFile('w', delete=False, suffix='.html') as f:
+        url = 'file://' + f.name
+        f.write(visual)
+    webbrowser.open(url)
+
 if __name__ == '__main__':
-    opt = get_config("config_files/en_filtered_config.yaml")
-    train(opt, amp=False)
+    #opt = get_config("config_files/en_filtered_config.yaml")
+    #train(opt, amp=False)
+    train_ner()
 
-
-r'''
-file = r'C:\Users\33669\PycharmProjects\OCR\pdf2img\page_3.jpg'
-pdf = pytesseract.image_to_pdf_or_hocr(file, config='--oem 1', extension='pdf')
-print(pytesseract.image_to_string(file))
-with open('C:\Users\33669\PycharmProjects\OCR\pdf2img\out_ocr1.pdf', 'w+b') as f:
-    f.write(pdf)
-'''
