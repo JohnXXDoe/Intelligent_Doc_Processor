9b3182530b584ab2e2ef9a439a027319a002b609
JohnXXDoe <38093875+JohnXXDoe@users.noreply.github.com>
2021-12-13 11:20:00 +0530

    v4.2
    Added Visualizer for final NER output
    Added direct pdf to NER support (no more need of pre tokenized sentences)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import csv
 
 import easyocr
-import pdf2image
-from flair.data import Sentence
 from pdf2image import convert_from_path  # For scanned PDFs
 from PIL import Image
 from pdfminer import high_level # Convert text PDF to text
@@ -13,8 +11,17 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from io import StringIO
-import nltk
 from flair.models import SequenceTagger
+from flair.tokenization import SegtokSentenceSplitter
+
+from unittest.mock import MagicMock
+from flair.data import Sentence, Span, Token
+from flair.visual import *
+from flair.visual.ner_html import render_ner_html, HTML_PAGE, TAGGED_ENTITY, PARAGRAPH
+import tempfile
+import webbrowser
+
+
 
 def pdf2img(PDF):
     # print(len(PDF))
@@ -83,270 +90,104 @@ def img_ocr(loc):  # For Image/Scanned PDF to text
     cv2.waitKey(0)
 
 
-def text_pdf(pdf):  # For PDF that is in text selectable format char_margin=30, line_margin=2, boxes_flow=1
+def ner(pdf):
+    sentences = []
+    tagger = SequenceTagger.load(
+        r'C:\Users\33669\PycharmProjects\OCR\trainer\resources\taggers\reg_train\final-model.pt')
+    print(tagger)
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
     codec = 'utf-8'
     laparams = LAParams(char_margin=30, line_margin=2, boxes_flow=1)
     device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
     interpreter = PDFPageInterpreter(rsrcmgr, device)
-    caching = True
-    pagenos = set()
+    rawdata = []
     fp = open(pdf, 'rb')
-    writer = csv.writer(open(r"H:\Code\Doc_IMG-OCR\Tender_Text1.csv", 'w', newline=''))
-    #file = open(r"H:\Code\Doc_IMG-OCR\Tender_Text1.txt", 'a')
-    #file.truncate(0)
+
     for pagenumber, page in enumerate(pdfpage.PDFPage.get_pages(fp, check_extractable=True)):
-        print(pagenumber)
-        if pagenumber == 150:
+        # print(pagenumber)
+        if pagenumber:
             interpreter.process_page(page)
             data = retstr.getvalue()
-            sent_text = nltk.sent_tokenize(data)
-            for sentence in sent_text:
-                tokenized_text = nltk.word_tokenize(sentence)
-                print(tokenized_text)
-                for word in tokenized_text:
-                    writer.writerow([word])
-                writer.writerow('\n')
-                #file.write(str(tokenized_text))
-    #file.close()
+            # cleansent = clean(sent)
+            encoded_string = data.encode("ascii", "ignore")
+            cleanpage = encoded_string.decode()
+            splitter = SegtokSentenceSplitter()
+            sentences = splitter.split(cleanpage)
+            tagger.predict(sentences)
+            for sentence in sentences:
+                print(sentence.to_tagged_string())
+    colors = {
+        "default": "#F7FF53",
+        "O": "#ddd",
+    }
+    actual = render_ner_html(sentences, colors=colors)
+    with tempfile.NamedTemporaryFile('w', delete=False, suffix='.html') as f:
+        url = 'file://' + f.name
+        f.write(actual)
+    webbrowser.open(url)
 
-def ner(file):
-    sentences = []
-    tagger = SequenceTagger.load('H:/Code/Doc_IMG-OCR/trainer/resources/taggers/ner-flert/final-model.pt')
-    with open(file, 'r') as read_obj:
-        for line in read_obj:
-            sentence = Sentence(line)
-            tagger.predict(sentence)
-            for entity in sentence.get_spans('ner'):
-                print(entity)
+def mock_ner_span(text, tag, start, end):
+    span = Span([]).set_label('class', tag)
+    span.start_pos = start
+    span.end_pos = end
+    span.tokens = [Token(text[start:end])]
+    return span
+
+
+def test_html_rendering():
+    text = (
+        "Boris Johnson has been elected new Conservative leader in a ballot of party members and will become the "
+        "next UK prime minister. &"
+    )
+    sent = Sentence()
+    sent.get_spans = MagicMock()
+    sent.get_spans.return_value = [
+        mock_ner_span(text, "PER", 0, 13),
+        mock_ner_span(text, "MISC", 35, 47),
+        mock_ner_span(text, "LOC", 109, 111),
+    ]
+    sent.to_original_text = MagicMock()
+    sent.to_original_text.return_value = text
+    colors = {
+        "PER": "#F7FF53",
+        "ORG": "#E8902E",
+        "LOC": "yellow",
+        "MISC": "#4647EB",
+        "O": "#ddd",
+    }
+    actual = render_ner_html([sent], colors=colors)
+
+    expected_res = HTML_PAGE.format(
+        text=PARAGRAPH.format(
+            sentence=TAGGED_ENTITY.format(
+                color="#F7FF53", entity="Boris Johnson", label="PER"
+            )
+                     + " has been elected new "
+                     + TAGGED_ENTITY.format(color="#4647EB", entity="Conservative", label="MISC")
+                     + " leader in a ballot of party members and will become the next "
+                     + TAGGED_ENTITY.format(color="yellow", entity="UK", label="LOC")
+                     + " prime minister. &amp;"
+        ),
+        title="Flair",
+    )
+
+    html = actual
+
+    with tempfile.NamedTemporaryFile('w', delete=False, suffix='.html') as f:
+        url = 'file://' + f.name
+        f.write(html)
+    webbrowser.open(url)
+    assert expected_res == actual
 
 
 if __name__ == '__main__':
-    PDF_file = r'H:\Code\Data\EIL Specs.pdf'
+    pdfname = 'AGRA'
+    PDF_file = r'C:\Users\33669\PycharmProjects\OCR\trainer\ner_data\raw/' + pdfname + '.pdf'
     img_loc = r'C:\Users\33669\PycharmProjects\OCR\pdf2img\K2.jpg'
     filename = 'PGCIL'
     # img_ocr(img_loc)
     # pdf2img(PDF_file)
     # searchable_ocr(img_loc) # For converting image to text embedded PDF
-    #text_pdf(PDF_file)
-    ner('H:/Code/Doc_IMG-OCR/trainer/ner/test/'+filename +'.csv')
-
-'''
-H:\Code\Doc_IMG-OCR\lib\Scripts\python.exe H:/Code/Doc_IMG-OCR/main.py
-2021-11-25 14:16:41,580 loading file H:/Code/Doc_IMG-OCR/trainer/resources/taggers/ner-flert/final-model.pt
-Span [3,4]: "xlpe insulated"   [− Labels: insulation (0.9851)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9991)]
-Span [10,11]: "pvc insulated"   [− Labels: insualtion (0.9992)]
-Span [3]: "stranded"   [− Labels: conductor (0.9965)]
-Span [3,4,5,6,7]: "cable size cable type 1"   [− Labels: marking (0.7834)]
-Span [4]: "xlpe"   [− Labels: insulation (0.999)]
-Span [5]: "xlpe"   [− Labels: insulation (0.999)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9989)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [3,4]: "xlpe insulated"   [− Labels: insulation (0.9851)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9991)]
-Span [10,11]: "pvc insulated"   [− Labels: insualtion (0.9992)]
-Span [3]: "stranded"   [− Labels: conductor (0.9965)]
-Span [3,4,5,6,7]: "cable size cable type 1"   [− Labels: marking (0.7834)]
-Span [4]: "xlpe"   [− Labels: insulation (0.999)]
-Span [5]: "xlpe"   [− Labels: insulation (0.999)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9989)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [4]: "xlpe"   [− Labels: insulation (0.9982)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [5]: "pvc"   [− Labels: inner (0.3307)]
-Span [3]: "pvc"   [− Labels: insulation (0.6103)]
-Span [3,4]: "xlpe insulated"   [− Labels: insulation (0.9851)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9991)]
-Span [10,11]: "pvc insulated"   [− Labels: insualtion (0.9992)]
-Span [3]: "stranded"   [− Labels: conductor (0.9965)]
-Span [3,4,5,6,7]: "cable size cable type 1"   [− Labels: marking (0.7834)]
-Span [4]: "xlpe"   [− Labels: insulation (0.999)]
-Span [5]: "xlpe"   [− Labels: insulation (0.999)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9989)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [4]: "xlpe"   [− Labels: insulation (0.9982)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [5]: "pvc"   [− Labels: inner (0.3307)]
-Span [3]: "pvc"   [− Labels: insulation (0.6103)]
-Span [19]: "contractor"   [− Labels: conductor (0.9558)]
-Span [23]: "contractor"   [− Labels: conductor (0.9426)]
-Span [3,4]: "xlpe insulated"   [− Labels: insulation (0.9851)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9991)]
-Span [10,11]: "pvc insulated"   [− Labels: insualtion (0.9992)]
-Span [3]: "stranded"   [− Labels: conductor (0.9965)]
-Span [3,4,5,6,7]: "cable size cable type 1"   [− Labels: marking (0.7834)]
-Span [4]: "xlpe"   [− Labels: insulation (0.999)]
-Span [5]: "xlpe"   [− Labels: insulation (0.999)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9989)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [4]: "xlpe"   [− Labels: insulation (0.9982)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [5]: "pvc"   [− Labels: inner (0.3307)]
-Span [3]: "pvc"   [− Labels: insulation (0.6103)]
-Span [19]: "contractor"   [− Labels: conductor (0.9558)]
-Span [23]: "contractor"   [− Labels: conductor (0.9426)]
-Span [2]: "xlpe"   [− Labels: insulation (0.9984)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9989)]
-Span [44,45,46]: "pvc insulated cables"   [− Labels: insualtion (0.729)]
-Span [51,52,53]: "xlpe insulated cables"   [− Labels: insualtion (0.7563)]
-Span [26]: "pvc"   [− Labels: insulation (0.6504)]
-Span [2,3]: "xlpe insulated"   [− Labels: insulation (0.9897)]
-Span [2,3]: "pvc insulated"   [− Labels: insualtion (0.9989)]
-Span [19]: "stranding"   [− Labels: conductor (0.9528)]
-Span [3,4]: "xlpe insulated"   [− Labels: insulation (0.9851)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9991)]
-Span [10,11]: "pvc insulated"   [− Labels: insualtion (0.9992)]
-Span [3]: "stranded"   [− Labels: conductor (0.9965)]
-Span [3,4,5,6,7]: "cable size cable type 1"   [− Labels: marking (0.7834)]
-Span [4]: "xlpe"   [− Labels: insulation (0.999)]
-Span [5]: "xlpe"   [− Labels: insulation (0.999)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9989)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [4]: "xlpe"   [− Labels: insulation (0.9982)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [5]: "pvc"   [− Labels: inner (0.3307)]
-Span [3]: "pvc"   [− Labels: insulation (0.6103)]
-Span [19]: "contractor"   [− Labels: conductor (0.9558)]
-Span [23]: "contractor"   [− Labels: conductor (0.9426)]
-Span [2]: "xlpe"   [− Labels: insulation (0.9984)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9989)]
-Span [44,45,46]: "pvc insulated cables"   [− Labels: insualtion (0.729)]
-Span [51,52,53]: "xlpe insulated cables"   [− Labels: insualtion (0.7563)]
-Span [26]: "pvc"   [− Labels: insulation (0.6504)]
-Span [2,3]: "xlpe insulated"   [− Labels: insulation (0.9897)]
-Span [2,3]: "pvc insulated"   [− Labels: insualtion (0.9989)]
-Span [19]: "stranding"   [− Labels: conductor (0.9528)]
-Span [7,8]: "pvc insulated"   [− Labels: insualtion (0.9988)]
-Span [1]: "xlpe"   [− Labels: insulation (0.9981)]
-Span [3]: "xlpe"   [− Labels: insulation (0.9794)]
-Span [3]: "xlpe"   [− Labels: insulation (0.9991)]
-Span [12,13]: "extruded pvc"   [− Labels: inner (0.5225)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [12,13]: "aluminium wires"   [− Labels: armouring (0.7419)]
-Span [17]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [1]: "pvc"   [− Labels: insulation (0.4401)]
-Span [7]: "insulated"   [− Labels: insualtion (0.5475)]
-Span [5]: "stranded"   [− Labels: conductor (0.9962)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [13,14]: "extruded pvc"   [− Labels: inner (0.7767)]
-Span [6]: "extruded"   [− Labels: inner (0.3093)]
-Span [7]: "pvc"   [− Labels: insulation (0.5888)]
-Span [1,2,3]: "pvc control cables"   [− Labels: inner (0.4062)]
-Span [7]: "insulated"   [− Labels: insualtion (0.9887)]
-Span [5]: "stranded"   [− Labels: conductor (0.9958)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [7]: "pvc"   [− Labels: insulation (0.5043)]
-Span [3,4]: "xlpe insulated"   [− Labels: insulation (0.9851)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9991)]
-Span [10,11]: "pvc insulated"   [− Labels: insualtion (0.9992)]
-Span [3]: "stranded"   [− Labels: conductor (0.9965)]
-Span [3,4,5,6,7]: "cable size cable type 1"   [− Labels: marking (0.7834)]
-Span [4]: "xlpe"   [− Labels: insulation (0.999)]
-Span [5]: "xlpe"   [− Labels: insulation (0.999)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9989)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [4]: "xlpe"   [− Labels: insulation (0.9982)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [5]: "pvc"   [− Labels: inner (0.3307)]
-Span [3]: "pvc"   [− Labels: insulation (0.6103)]
-Span [19]: "contractor"   [− Labels: conductor (0.9558)]
-Span [23]: "contractor"   [− Labels: conductor (0.9426)]
-Span [2]: "xlpe"   [− Labels: insulation (0.9984)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9989)]
-Span [44,45,46]: "pvc insulated cables"   [− Labels: insualtion (0.729)]
-Span [51,52,53]: "xlpe insulated cables"   [− Labels: insualtion (0.7563)]
-Span [26]: "pvc"   [− Labels: insulation (0.6504)]
-Span [2,3]: "xlpe insulated"   [− Labels: insulation (0.9897)]
-Span [2,3]: "pvc insulated"   [− Labels: insualtion (0.9989)]
-Span [19]: "stranding"   [− Labels: conductor (0.9528)]
-Span [7,8]: "pvc insulated"   [− Labels: insualtion (0.9988)]
-Span [1]: "xlpe"   [− Labels: insulation (0.9981)]
-Span [3]: "xlpe"   [− Labels: insulation (0.9794)]
-Span [3]: "xlpe"   [− Labels: insulation (0.9991)]
-Span [12,13]: "extruded pvc"   [− Labels: inner (0.5225)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [12,13]: "aluminium wires"   [− Labels: armouring (0.7419)]
-Span [17]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [1]: "pvc"   [− Labels: insulation (0.4401)]
-Span [7]: "insulated"   [− Labels: insualtion (0.5475)]
-Span [5]: "stranded"   [− Labels: conductor (0.9962)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [13,14]: "extruded pvc"   [− Labels: inner (0.7767)]
-Span [6]: "extruded"   [− Labels: inner (0.3093)]
-Span [7]: "pvc"   [− Labels: insulation (0.5888)]
-Span [1,2,3]: "pvc control cables"   [− Labels: inner (0.4062)]
-Span [7]: "insulated"   [− Labels: insualtion (0.9887)]
-Span [5]: "stranded"   [− Labels: conductor (0.9958)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [7]: "pvc"   [− Labels: insulation (0.5043)]
-Span [56,57]: "xlpe insulated"   [− Labels: insualtion (0.9989)]
-Span [41,42]: "xlpe insulated"   [− Labels: insualtion (0.9988)]
-Span [22]: "contractor"   [− Labels: conductor (0.5463)]
-Span [5,6]: "xlpe insulated"   [− Labels: insualtion (0.9596)]
-Span [18]: "voltage"   [− Labels: marking (0.5689)]
-Span [20,21]: "xlpe insulated"   [− Labels: insualtion (0.9988)]
-Span [3,4]: "xlpe insulated"   [− Labels: insulation (0.9851)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9991)]
-Span [10,11]: "pvc insulated"   [− Labels: insualtion (0.9992)]
-Span [3]: "stranded"   [− Labels: conductor (0.9965)]
-Span [3,4,5,6,7]: "cable size cable type 1"   [− Labels: marking (0.7834)]
-Span [4]: "xlpe"   [− Labels: insulation (0.999)]
-Span [5]: "xlpe"   [− Labels: insulation (0.999)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9989)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [4]: "xlpe"   [− Labels: insulation (0.9982)]
-Span [6]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [7]: "xlpe"   [− Labels: insulation (0.9979)]
-Span [5]: "pvc"   [− Labels: inner (0.3307)]
-Span [3]: "pvc"   [− Labels: insulation (0.6103)]
-Span [19]: "contractor"   [− Labels: conductor (0.9558)]
-Span [23]: "contractor"   [− Labels: conductor (0.9426)]
-Span [2]: "xlpe"   [− Labels: insulation (0.9984)]
-Span [4,5]: "pvc insulated"   [− Labels: insualtion (0.9989)]
-Span [44,45,46]: "pvc insulated cables"   [− Labels: insualtion (0.729)]
-Span [51,52,53]: "xlpe insulated cables"   [− Labels: insualtion (0.7563)]
-Span [26]: "pvc"   [− Labels: insulation (0.6504)]
-Span [2,3]: "xlpe insulated"   [− Labels: insulation (0.9897)]
-Span [2,3]: "pvc insulated"   [− Labels: insualtion (0.9989)]
-Span [19]: "stranding"   [− Labels: conductor (0.9528)]
-Span [7,8]: "pvc insulated"   [− Labels: insualtion (0.9988)]
-Span [1]: "xlpe"   [− Labels: insulation (0.9981)]
-Span [3]: "xlpe"   [− Labels: insulation (0.9794)]
-Span [3]: "xlpe"   [− Labels: insulation (0.9991)]
-Span [12,13]: "extruded pvc"   [− Labels: inner (0.5225)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [12,13]: "aluminium wires"   [− Labels: armouring (0.7419)]
-Span [17]: "xlpe"   [− Labels: insulation (0.9988)]
-Span [1]: "pvc"   [− Labels: insulation (0.4401)]
-Span [7]: "insulated"   [− Labels: insualtion (0.5475)]
-Span [5]: "stranded"   [− Labels: conductor (0.9962)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [13,14]: "extruded pvc"   [− Labels: inner (0.7767)]
-Span [6]: "extruded"   [− Labels: inner (0.3093)]
-Span [7]: "pvc"   [− Labels: insulation (0.5888)]
-Span [1,2,3]: "pvc control cables"   [− Labels: inner (0.4062)]
-Span [7]: "insulated"   [− Labels: insualtion (0.9887)]
-Span [5]: "stranded"   [− Labels: conductor (0.9958)]
-Span [6]: "armoured"   [− Labels: conductor (0.4698)]
-Span [7]: "pvc"   [− Labels: insulation (0.5043)]
-Span [56,57]: "xlpe insulated"   [− Labels: insualtion (0.9989)]
-Span [41,42]: "xlpe insulated"   [− Labels: insualtion (0.9988)]
-Span [22]: "contractor"   [− Labels: conductor (0.5463)]
-Span [5,6]: "xlpe insulated"   [− Labels: insualtion (0.9596)]
-Span [18]: "voltage"   [− Labels: marking (0.5689)]
-Span [20,21]: "xlpe insulated"   [− Labels: insualtion (0.9988)]
-Span [13,14]: "galvanized steel"   [− Labels: armouring (0.7288)]
-Span [24,25]: "extruded pvc"   [− Labels: inner (0.6097)]
-Span [3]: "xlpe"   [− Labels: insulation (0.9992)]
-Span [24,25]: "xlpe insulated"   [− Labels: insulation (0.9835)]
-Span [42]: "xlpe"   [− Labels: insulation (0.9424)]
-
-Process finished with exit code 0
-'''
+    ner(PDF_file)
+    #test_html_rendering()
