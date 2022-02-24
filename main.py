7553ac4782fa95e0455e830a46a0c3550e3ed0f2
Utkarsh Kharayat <utkarshkhrt69@gmail.com>
2022-02-24 09:22:13 +0530

    Added sentence to output summary along with detected text.

--- main.py
+++ main.py
@@ -24,10 +24,13 @@ import tempfile
 import webbrowser
 
 
-def pdf2img(PDF):
+
+
+def pdf2img(PDF, name):
     # print(len(PDF))
-    pages = convert_from_path(PDF, 500, poppler_path=r"C:\poppler-21.09.0\Library\bin", first_page=150,
-                              last_page=160, timeout=10000)
+    global total_pages
+    pages = convert_from_path(PDF, 500, poppler_path=r"C:\poppler-0.68.0\bin", timeout=10000)
+
     #
     # # Counter to store images of each page of PDF to image
     image_counter = 1
@@ -36,66 +39,70 @@ def pdf2img(PDF):
     for page in pages:
         # Declaring filename for each page of PDF as JPG
         # For each page, filename will be:
-        # PDF page 1 -> page_1.jpg
-        # PDF page 2 -> page_2.jpg
-        # PDF page 3 -> page_3.jpg
-        # ....
-        # PDF page n -> page_n.jpg
-        filename = "page" + str(image_counter) + ".png"
+        # PDF page 1 -> pdfname_1.jpg
+        # PDF page 2 -> pdfname_2.jpg
+        # PDF page 3 -> pdfname_3.jpg
+        # . . . .
+        # PDF page n -> pdfname_n.jpg
+        filename = f'{name}_' + str(image_counter) + ".png"
 
         # Save the image of the page in system
-        page.save(r'C:\Users\33669\PycharmProjects\OCR\pdf2img\ ' + filename)
+        page.save(r'C:\Data\Output\OCR\images/' + filename)
         print('Saved page number ' + str(image_counter))
         # Increment the counter to update filename
         image_counter = image_counter + 1
+    total_pages = image_counter
 
 
 def searchable_ocr(img):  # From image to searchable PDF
     pdf = pytesseract.image_to_pdf_or_hocr(img, config='--oem 1', extension='pdf')
     print(pytesseract.image_to_string(img))
-    with open(r'C:\Users\33669\PycharmProjects\OCR\outputs\Searchable.pdf', 'w+b') as f:
+    with open(r'C:\Data\test\Searchable.pdf', 'w+b') as f:
         f.write(pdf)
 
 
-def img_ocr(loc):  # For Image/Scanned PDF to text
-    image = cv2.imread(loc)
-    reader = easyocr.Reader(['en'],
-                            recog_network='custom_example')  # , recog_network='custom_example' this needs to run only once to load the model into memory
-    result = reader.readtext(img_loc,
-                             paragraph=True)  # , rotation_info=[90, 180, 270], y_ths=1, x_ths=0.09, height_ths=0.5, ycenter_ths=0.5, width_ths=0.5
-    cv2.startWindowThread()
-    for (bbox, text) in result:  # , prob
-        # display the OCR'd text and associated probability
-        # print("[INFO] {:.4f}: {}".format(prob, text))
-        print(text)
-        # unpack the bounding box
-        (tl, tr, br, bl) = bbox
-        tl = (int(tl[0]), int(tl[1]))
-        tr = (int(tr[0]), int(tr[1]))
-        br = (int(br[0]), int(br[1]))
-        bl = (int(bl[0]), int(bl[1]))
-        # cleanup the text and draw the box surrounding the text along
-        # with the OCR'd text itself
-        cv2.rectangle(image, tl, br, (0, 0, 255), 4)
-        cv2.putText(image, text, (tl[0], tl[1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0, 90, 200), 8)
+def img_ocr(location, filename):  # For Image/Scanned PDF to text
+    for page in tqdm(range(1, total_pages), desc='Converting images to text. . .'):
+        loc = f'{location}/{filename}_{page}.png'
+        image = cv2.imread(loc)
+        reader = easyocr.Reader(['en'],
+                                recog_network='custom_example')  # , recog_network='custom_example' this needs to run only once to load the model into memory
+        result = reader.readtext(loc, rotation_info=[90, 180, 270], y_ths=1, x_ths=0.09, height_ths=0.5, ycenter_ths=0.5, width_ths=0.5)
 
-    file = open(r"C:\Users\33669\PycharmProjects\OCR\outputs\OCR_OUT.txt", 'w')
-    for (bbox, text) in result:  # , prob
-        file.write(str(text))
-        file.write('\n')
-    file.close()
-    # show the output image
-    cv2.namedWindow('PDF Output', cv2.WINDOW_NORMAL)
-    cv2.imshow("PDF Output", image)
-    cv2.waitKey(0)
+                                 #paragraph=True)  # , rotation_info=[90, 180, 270], y_ths=1, x_ths=0.09, height_ths=0.5, ycenter_ths=0.5, width_ths=0.5
+        cv2.startWindowThread()
+        for (bbox, text, prob) in result:  # , prob
+            # display the OCR'd text and associated probability
+            # print("[INFO] {:.4f}: {}".format(prob, text))
+            print(text)
+            # unpack the bounding box
+            (tl, tr, br, bl) = bbox
+            tl = (int(tl[0]), int(tl[1]))
+            tr = (int(tr[0]), int(tr[1]))
+            br = (int(br[0]), int(br[1]))
+            bl = (int(bl[0]), int(bl[1]))
+            # cleanup the text and draw the box surrounding the text along
+            # with the OCR'd text itself
+            cv2.rectangle(image, tl, br, (0, 0, 255), 4)
+            cv2.putText(image, text, (tl[0], tl[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0, 90, 200), 8)
+
+        file = open(f"C:/Data/Output/OCR/{filename}_OCR.txt", 'a')
+        for (bbox, text, prob) in result:  # , prob
+            file.write(str(text))
+            file.write('\n')
+        file.close(200)
+        # show the output image
+        cv2.namedWindow('PDF Output', cv2.WINDOW_NORMAL)
+        cv2.imshow("PDF Output", image)
+        cv2.waitKey(0)
 
 
 def ner(pdf, titles, limit):
     i = 1
     sentences = []
     tagger = SequenceTagger.load(
-        r'E:\PycharmProjects\DL\Doc_IMG-OCR\trainer\resources\taggers\full-fixed-roberta-base\best-model.pt') #all-fixed-roberta-base-resume
+        r'E:\PycharmProjects\DL\Doc_IMG-OCR\trainer\resources\taggers\full-fixed-roberta-base\best-model.pt')  # all-fixed-roberta-base-resume
     print(tagger)
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
@@ -113,8 +120,19 @@ def ner(pdf, titles, limit):
     ##############
 
     for pagenum, page in enumerate(pdfpage.PDFPage.get_pages(fp, check_extractable=True)):
-        if pagenum:
+        if pagenum is not None:
             interpreter.process_page(page)
+            if len(retstr.getvalue()) < 10:
+                print(f'>> OCR PAGE >>{retstr.getvalue()} <<<<<<< Page number: {pagenum + 1}<<<<< ! ! ! ')
+                # Page is OCR only
+
+            else:
+                data += retstr.getvalue().decode('ascii', 'ignore')
+                data = data.replace('\x0c', ' ')
+                print(f'::PAGE IS NORMAL AND EXTRACTABLE::')
+            retstr.truncate(0)
+            retstr.seek(0)
+        pagenum += 1
         pbar.update(1)
     pbar.close()
     data = retstr.getvalue()
@@ -137,17 +155,17 @@ def ner(pdf, titles, limit):
         print(f'//  Text ,   Entity Tag ,  Confidence percentage   //')
         f.writelines(f'//////////////////////////////////////////////////////////////////////////////// \n')
         f.writelines(f'//////////////////////////////////////////////////////////////////////////////// \n')
-        f.writelines(f'//////////////////  E X T R A C T I O N    R E S U L T  /////////////////////// \n')
+        f.writelines(f'//////////////////  E X T R A C T I O N    R E S U L T  //////////////////////// \n')
         f.writelines(f'//////////////////////////////////////////////////////////////////////////////// \n')
         f.writelines(f'//////////////////////////////////////////////////////////////////////////////// \n')
-        f.writelines(f'//  Text ,   Entity Tag ,  Confidence percentage   //')
+        f.writelines(f'Text, Entity-[Confidence]')
         f.writelines(f'------------------------------------------------------------------------------- \n')
         for sentence in sentences:
             for entity in sentence.get_spans('ner', min_score=threshold):
-                f.writelines(f'> {entity.text}, {entity.tag}[{(round(entity.score, 4) * 100)}% ] \n')
-                f.writelines(f'>> {sentence.to_original_text()}, {entity.tag} \n')
-                print(f'// =={entity.text}  ====  {entity.tag} :::: {(round(entity.score, 4) * 100)}% :::://')
-                print(f'// =={sentence.to_original_text()}  ====  {entity.tag} :::: :::://')
+                if str(entity.tag) != 'tenderid':
+                    f.writelines(f'> {entity.text}, {entity.tag}-[{(round(entity.score, 4) * 100)}%] \n')
+                    f.writelines(f'>> {sentence.to_original_text()}, {entity.tag} \n\n')
+                    print(f'// =={entity.text}  ====  {entity.tag} :::: {(round(entity.score, 4) * 100)}% :::://')
         print(f'|______________________________________________________________________________|')
 
     colors = {
@@ -165,7 +183,7 @@ def ner(pdf, titles, limit):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', dest='threshold', help='Default is to show all tags  , Limit: [0,1]', type=float)
+    parser.add_argument('-c', dest='threshold', help='Default is to show all tags  , Limit: [0,1]', type=float, default=-1)
     parser.add_argument('-f', dest='pdfname', help='Name of PDF to be processed', type=str)
     args = parser.parse_args()
 
@@ -188,13 +206,13 @@ if __name__ == '__main__':
     pdfname = args.pdfname
 
     # pdfname = 'PGCIL'#PGCIL BSES TENDER EIL Specs BHEL
-    PDF_file = r'C:\Data\test/' + pdfname + '.pdf'
-    img_loc = r'C:\Users\33669\PycharmProjects\OCR\pdf2img\K2.jpg'
+    PDF_file = f'C:/Data/test/{pdfname}.pdf'
+    img_loc = r'C:\Data\Output\OCR\images'
 
-    # img_ocr(img_loc)
-    # pdf2img(PDF_file)
-    # searchable_ocr(img_loc) # For converting image to text embedded PDF
-    ner(PDF_file, pdfname, threshold)
+    pdf2img(PDF_file, pdfname)
+    img_ocr(img_loc, pdfname)
+    # searchable_ocr(img_loc)  # For converting image to text embedded PDF
+    # ner(PDF_file, pdfname, threshold)
 
 '''
 CLI command :
